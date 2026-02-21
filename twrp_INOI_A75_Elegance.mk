8eb3b801426b6fc2a18bef081d2d229cf77e00e7
tommynok <69355171+tommynok@users.noreply.github.com>
2026-02-21 05:26:14 +0300

    Update product inheritance in device makefile

--- twrp_INOI_A75_Elegance.mk
+++ twrp_INOI_A75_Elegance.mk
@@ -5,10 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/otrp/config/common.mk)
+
+# Inherit from INOI_A75_Elegance device
 $(call inherit-product, device/inoi_limited/INOI_A75_Elegance/device.mk)
 
 PRODUCT_DEVICE := INOI_A75_Elegance
