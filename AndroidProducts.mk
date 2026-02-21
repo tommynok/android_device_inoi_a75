342943791f63ea4399e65e695f269ec2aeb114e4
tommynok <69355171+tommynok@users.noreply.github.com>
2026-02-21 19:20:56 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_INOI_A75_Elegance.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_INOI_A75_Elegance-ap2a-eng \
-    twrp_INOI_A75_Elegance-ap2a-userdebug
+    twrp_INOI_A75_Elegance-ap3a-eng \
+    twrp_INOI_A75_Elegance-ap3a-userdebug
 
