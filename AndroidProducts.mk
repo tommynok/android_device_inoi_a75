02ef7489d1c6d52ba4754e04ca166623ce5e637e
tommynok <69355171+tommynok@users.noreply.github.com>
2026-02-21 21:44:10 +0300

    Modify AndroidProducts.mk for TWRP device
    Updated product makefiles and lunch choices for TWRP.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_INOI_A75_Elegance.mk \
-    $(LOCAL_DIR)/omni_INOI_A75_Elegance.mk
+    $(LOCAL_DIR)/twrp_INOI_A75_Elegance.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_INOI_A75_Elegance-ap3a-eng \
-    omni_INOI_A75_Elegance-ap3a-userdebug
+    twrp_INOI_A75_Elegance-eng \
+    twrp_INOI_A75_Elegance-userdebug
